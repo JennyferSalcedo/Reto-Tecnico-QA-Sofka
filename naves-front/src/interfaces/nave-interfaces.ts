1b8c795a4031f417616f0c5f85e39c862b82aca0
Jennyfer Salcedo <jennyferroa1997@gmail.com>
2023-02-07 13:53:29 -0500

    Creé las interfaces para cada una de las naves con sus respectivas abstracciones.

---
+++ naves-front/src/interfaces/nave-interfaces.ts
@@ -0,0 +1,29 @@
+// Nave Espacial
+export interface Spaceship {
+  name: string;
+  weight: number;
+}
+
+// Nave Espacial con empuje
+export interface ThrustSpaceship extends Spaceship {
+  pushingPower: number;
+}
+
+// Vehículo lanzadera
+export interface ShuttleVehicle extends ThrustSpaceship {
+  loadedWeight: number;
+  height: number;
+  fuel: string;
+}
+
+// Naves espaciales no tripuladas
+export interface UnmannedSpaceCraft extends ThrustSpaceship {
+  speed: string;
+  hasCells: boolean;
+}
+
+// Naves espaciales tripuladas
+export interface MannedSpaceCraft extends Spaceship {
+  groundClearance: string;
+  numberOfSeats: boolean;
+}
