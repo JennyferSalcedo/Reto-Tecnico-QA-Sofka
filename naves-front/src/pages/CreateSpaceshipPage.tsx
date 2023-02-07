88b7de40ce57ac683cbc4cb646ed8e9a164c85ef
Jennyfer Salcedo <jennyferroa1997@gmail.com>
2023-02-07 17:00:22 -0500

    Se empieza con la modificación y estilización de <CreateSpaceshipPage />

--- naves-front/src/pages/CreateSpaceshipPage.tsx
+++ naves-front/src/pages/CreateSpaceshipPage.tsx
@@ -1,6 +1,14 @@
-
 export const CreateSpaceshipPage = () => {
   return (
-    <div>CreateSpaceShipPage</div>
-  )
-}
+    <div className="mainContainer">
+      <div className="formContainer">
+        <form>
+          <h3>
+            ¿Eres un apasionado por el espacio y quieres crear tu propio
+            inventario de naves espaciales?
+          </h3>
+        </form>
+      </div>
+    </div>
+  );
+};
